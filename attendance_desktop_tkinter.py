21d15c70bb30083f49fed806563062848e9352d6
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-10-24 17:26:00 +0600

    Ibiofin-gui

--- attendance_desktop_tkinter.py
+++ attendance_desktop_tkinter.py
@@ -6,8 +6,17 @@ from PIL import Image,ImageTk
 class Face_Recognition_System:
     def __init__(self,root):
         self.root=root
-        self.root.geometry("1530x790+0+0") #0 denote x, y axis
+        self.root.geometry("1000x390+0+0") #0 denote x, y axis
         self.root.title("I-BioFin")
+
+        frame=Frame(self.root,bg="black")
+        frame.place(x=610,y=170,width=340,height=450)
+
+        img1=Image.open(r"C:\Users\rakib\OneDrive\Documents\GitHub\python-desktop-app_projects\Images\login-icon.png")
+        img1=img1.resize((100,100),Image.ANTIALIAS)
+        self.photoimage1=ImageTk.PhotoImage(img1)
+        lblimg1=Label(image=self.photoimage1,bg="black",borderwidth=0)
+        lblimg1.place(x=730,y=175,width=100,height=100)
 
 
 
